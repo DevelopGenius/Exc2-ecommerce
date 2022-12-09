0cb8a66a5e07521cc61234d76523b9a411acb61a
ariel <ariel3332@gmail.com>
2022-12-09 20:49:57 +0200

    Adding connection to mongo db

--- exc2-ecommerce-server/index.js
+++ exc2-ecommerce-server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3001;
 
@@ -6,6 +7,13 @@ app.use((req, res) => {
     console.log("we got a new request");
     res.send("HELLO");
 });
+
+mongoose.connect('mongodb://127.0.0.1:27017/ecommerce')
+    .then(() => {
+        console.log("mongo connection opened");
+    }).catch(err => {
+        console.log(`failed to open connection: `, err)
+    })
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}!`);
